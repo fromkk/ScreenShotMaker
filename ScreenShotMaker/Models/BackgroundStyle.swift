cc6c8b300576e0f9f158b14c43e9cb1dc2eeafc4
Kazuya Ueoka <info@fromkk.info>
2026-02-09 20:13:06 +0900

    feat: Add cross-platform compatibility helpers
    - Create PlatformCompatibility.swift with PlatformImage typealias (NSImage/UIImage)
    - Add Image(platformImage:) initializer for cross-platform image display
    - Add Color.platformBackground/platformControlBackground/platformControl/platformSeparator
    - Add FontHelper.availableFontFamilies (NSFontManager/UIFont.familyNames)
    - Update Color.toHex() in BackgroundStyle.swift with #if canImport branching
    
    Closes #042

--- ScreenShotMaker/Models/BackgroundStyle.swift
+++ ScreenShotMaker/Models/BackgroundStyle.swift
@@ -33,12 +33,22 @@ extension Color {
     }
 
     func toHex() -> String {
+        #if canImport(AppKit)
         guard let nsColor = NSColor(self).usingColorSpace(.sRGB) else {
             return "#000000"
         }
         let r = Int(round(nsColor.redComponent * 255))
         let g = Int(round(nsColor.greenComponent * 255))
         let b = Int(round(nsColor.blueComponent * 255))
+        #elseif canImport(UIKit)
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        UIColor(self).getRed(&red, green: &green, blue: &blue, alpha: nil)
+        let r = Int(round(red * 255))
+        let g = Int(round(green * 255))
+        let b = Int(round(blue * 255))
+        #endif
         return String(format: "#%02X%02X%02X", r, g, b)
     }
 }
---
+++ ScreenShotMaker/Utils/PlatformCompatibility.swift
@@ -0,0 +1,79 @@
+import SwiftUI
+
+#if canImport(AppKit)
+import AppKit
+#elseif canImport(UIKit)
+import UIKit
+#endif
+
+// MARK: - Platform Image
+
+#if canImport(AppKit)
+public typealias PlatformImage = NSImage
+#elseif canImport(UIKit)
+public typealias PlatformImage = UIImage
+#endif
+
+extension Image {
+    /// Create a SwiftUI Image from a platform-native image (NSImage on macOS, UIImage on iOS)
+    init(platformImage: PlatformImage) {
+        #if canImport(AppKit)
+        self.init(nsImage: platformImage)
+        #elseif canImport(UIKit)
+        self.init(uiImage: platformImage)
+        #endif
+    }
+}
+
+// MARK: - Platform Colors
+
+extension Color {
+    /// Window/screen background color
+    static var platformBackground: Color {
+        #if canImport(AppKit)
+        Color(nsColor: .windowBackgroundColor)
+        #elseif canImport(UIKit)
+        Color(uiColor: .systemBackground)
+        #endif
+    }
+
+    /// Control background color
+    static var platformControlBackground: Color {
+        #if canImport(AppKit)
+        Color(nsColor: .controlBackgroundColor)
+        #elseif canImport(UIKit)
+        Color(uiColor: .systemBackground)
+        #endif
+    }
+
+    /// Control color
+    static var platformControl: Color {
+        #if canImport(AppKit)
+        Color(nsColor: .controlColor)
+        #elseif canImport(UIKit)
+        Color(uiColor: .secondarySystemBackground)
+        #endif
+    }
+
+    /// Separator color
+    static var platformSeparator: Color {
+        #if canImport(AppKit)
+        Color(nsColor: .separatorColor)
+        #elseif canImport(UIKit)
+        Color(uiColor: .separator)
+        #endif
+    }
+}
+
+// MARK: - Font Helper
+
+enum FontHelper {
+    /// Available font family names sorted alphabetically
+    static var availableFontFamilies: [String] {
+        #if canImport(AppKit)
+        NSFontManager.shared.availableFontFamilies.sorted()
+        #elseif canImport(UIKit)
+        UIFont.familyNames.sorted()
+        #endif
+    }
+}
